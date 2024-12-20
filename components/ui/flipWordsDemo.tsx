4e5687c64bb8801a10910f0dd42b9386a0fe783d
Sukarn <sukarn@designingsolutions.co.in>
2024-12-20 13:27:02 +0530

    Changes: Hero section responsive

--- components/ui/flipWordsDemo.tsx
+++ components/ui/flipWordsDemo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlipWords } from "../ui/flip-words";
+import { motion } from "framer-motion";
 
 export function FlipWordsDemo() {
   const words = [
@@ -12,7 +13,7 @@ export function FlipWordsDemo() {
   ];
 
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
+    <div className= "h-[40rem] flex justify-center items-center px-4">
       <div className="text-lg mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Empowering
         <FlipWords words={words} /> <br />
